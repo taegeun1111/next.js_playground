7f0ef6021599a8c6eee6ed7721052388791d4745
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2025-02-09 18:57:35 +0900

    CHORE: install db.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,10 +18,10 @@ export default function RootLayout({
         </h1>
         <ol>
           <li>
-            <a href="read/1">html</a>
+            <a href="/read/1">html</a>
           </li>
           <li>
-            <a href="read/2">css</a>
+            <a href="/read/2">css</a>
           </li>
         </ol>
         {children}
