7479108013c8ee3316a99f62117e6fb6811846a3
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2025-02-18 21:59:40 +0900

    FEAT: fetching data

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,29 @@
+"use client";
 import { Metadata } from "next";
+import { useEffect, useState } from "react";
 
-export const metadata: Metadata = {
-  title: "MainPage",
-};
+// export const metadata: Metadata = {
+//   title: "Home",
+// };
 
 export default function Home() {
-  return <div>123</div>;
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://nomad-movies.nomadcoders.workers.dev/movies"
+    );
+
+    const json = await response.json();
+    setMovies(json);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div>{isLoading ? <div>loading...</div> : JSON.stringify(movies)}</div>
+  );
 }
